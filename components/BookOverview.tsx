1d33527f5da458a6fe861843fa6aa8f78fea6a9e
jagdish singh boura <bourajagdish1221@gmail.com>
2025-03-16 01:53:09 +0530

    dskf

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -26,10 +26,9 @@ const BookOverview = async ({
 }: Props) => {
   const [user] = await db.select().from(users).where(eq(users.id , userId)).limit(1);
 
-  if(!user)return null
 
   const borrowingEligibility={
-    isEligible:availableCopies>0 && user.status==='APPROVED',
+    isEligible:availableCopies>0 && user?.status==='APPROVED',
     message:availableCopies<=0?"Book is not AVailale ":"You are not Eligible to borrow this book",
   }
   return (
@@ -57,7 +56,7 @@ const BookOverview = async ({
           </p>
         </div>
         <p className="book-description">{description}</p>
-         <BorrowBook bookId={id} userId={userId}  borrowingEligibility={borrowingEligibility}/>
+         { user && <BorrowBook bookId={id} userId={userId}  borrowingEligibility={borrowingEligibility}/>}
       </div>
       <div className="relative flex flex-1 justify-center">
         <div className="relative">
